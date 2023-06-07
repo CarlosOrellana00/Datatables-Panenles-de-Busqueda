d4b5fdf66d9002943e7cb92fa851cd10d00040b4
CarlosOrellana00 <carlosorellana.oa@gmail.com>
2023-06-07 19:30:10 -0400

    4

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $('#example').DataTable({
+      searchPanes:{
+        cascadePanes:true,
+              dtOpts:{
+              dom:'tp',
+              paging:'true',
+              pagingType:'numbers', //'numbers' o 'simple'
+              searching:false //true or false (busqueda que selecciones)
+      }
+    },
+      dom:'Pfrtip',
+      columnDefs:[{
+        searchPanes:{
+        show:false
+      },
+        targets:[5]
+      }]
+  });
+});
